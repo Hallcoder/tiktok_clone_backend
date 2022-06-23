43887c8b5e0b2d50a2ca56878c3717754e049b9f
Hallcoder <hallcoder25@gmail.com>
2022-06-23 11:39:14 +0200

    Changed the connection url

--- utils/db.js
+++ utils/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 module.exports.db =  async()=>{
   return  await mongoose.connect(process.env.DB_URL)
-        .then(_=> console.log('connected'))
+        .then(_=> console.log('connected to the database'))
         .catch(error => console.error(error))
 }
