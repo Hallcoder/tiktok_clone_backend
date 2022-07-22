093cecf20b275cd659bec696e2c433d06a15f940
Hallcoder <hallcoder25@gmail.com>
2022-07-22 20:56:22 +0200

    configured origins to ease keep both development and production processes smooth

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const { post } = require('./routers/posts');
 const { accounts } = require('./routers/accounts');
 const app = express();
 const PORT= process.env.PORT;
+const allowedOrigins = ['https://tiktok-clone2.vercel.app','http://localhost:3000']
 app.use(express.json({limit:'250mb'}))
 app.use(function(req,res,next){
-    cors({origin:'https://tiktok-clone2.vercel.app'})
-    res.header('Access-Control-Allow-Origin', 'https://tiktok-clone2.vercel.app')
+    cors({origin:allowedOrigins.includes(req.headers.origin) ? req.headers.origin : 'https://tiktok-clone2.vercel.app'})
+    res.header('Access-Control-Allow-Origin',allowedOrigins.includes(req.headers.origin) ? req.headers.origin : 'https://tiktok-clone2.vercel.app')
     res.header('Access-Control-Allow-Methods', 'PUT,GET,POST')
     res.header('Access-Control-Allow-Headers', 'X-Request-With,Accept,Content-Type,X-HTTP-Method-Override')
     res.header('Access-Control-Allow-Credentials', true)
