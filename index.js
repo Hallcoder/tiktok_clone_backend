6cb5d3d64ce2d769e0e48fbba041a609aa78f383
Hallcoder <hallcoder25@gmail.com>
2022-08-16 12:41:55 +0200

    changed origin for production purposes

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const cors = require('cors');
 const { post } = require('./routers/posts');
 const { accounts } = require('./routers/accounts');
 const app = express();
-const PORT= process.env.PORT;
-const allowedOrigins = ['https://tiktok-clone2.vercel.app','http://localhost:3000']
+const PORT= process.env.PORT || 4000;
 app.use(express.json({limit:'250mb'}))
 app.use(function(req,res,next){
-    cors({origin:allowedOrigins.includes(req.headers.origin) ? req.headers.origin : 'https://tiktok-clone2.vercel.app'})
-    res.header('Access-Control-Allow-Origin',allowedOrigins.includes(req.headers.origin) ? req.headers.origin : 'https://tiktok-clone2.vercel.app')
+    cors({origin:req.headers.origin})
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Methods', 'PUT,GET,POST')
     res.header('Access-Control-Allow-Headers', 'X-Request-With,Accept,Content-Type,X-HTTP-Method-Override')
     res.header('Access-Control-Allow-Credentials', true)
